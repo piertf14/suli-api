bafbcc40b3c59ecde880ca396b2fb9c2636f6236
Piero Terreros <piero.terreros@gmail.com>
2018-06-16 06:08:25 -0500

    Add config

--- suli/settings.py
+++ suli/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -84,10 +85,7 @@ WSGI_APPLICATION = 'suli.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(),
 }
 
 
