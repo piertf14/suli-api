6e9e49a58e37c43c4694e7ae5ffbe4949b290dce
Piero Terreros <piero.terreros@gmail.com>
2018-07-15 02:48:36 -0500

    Add v2 of measurement-value

--- evaluation/serializers.py
+++ evaluation/serializers.py
@@ -37,3 +37,30 @@ class ReferentialImageSerializers(serializers.ModelSerializer):
     class Meta:
         model = ReferentialImage
         fields = '__all__'
+
+
+class MeasurementValueV2Serializers(serializers.ModelSerializer):
+    referential_image = serializers.SerializerMethodField()
+
+    def get_referential_image(self, obj):
+        referential_image = obj.measurement_value_referential_image.all()
+        if referential_image.exists():
+            serializer = ReferentialImageSerializers(referential_image.first(), many=False)
+            return serializer.data
+        return None
+
+    class Meta:
+        model = MeasurementValue
+        fields = '__all__'
+
+
+def measurement_value_parse_data(args):
+    dict = {}
+    dict['chain_custody'] = args.data['chain_custody']
+    dict['max'] = args.data['max']
+    dict['min'] = args.data['min']
+    dict['avg'] = args.data['avg']
+    dict['point_reference'] = args.data['point_reference']
+    dict['observation_measurement'] = args.data['observation_measurement']
+    dict['type_lighting'] = args.data['type_lighting']
+    return dict
--- evaluation/urls.py
+++ evaluation/urls.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
-from .views import AgentAPI, CustodyChainAPI, MeasurementValueAPI, ReferentialImageAPI
+from .views import AgentAPI, CustodyChainAPI, MeasurementValueAPI, ReferentialImageAPI, MeasurementValueV2API
 
 urls_patterns = [
     url(r'^agent/$', AgentAPI.as_view(), name='agent'),
     url(r'^custody-chain/$', CustodyChainAPI.as_view(), name='custody_chain'),
     url(r'^measurement-value/$', MeasurementValueAPI.as_view(), name='measurement_value'),
     url(r'^referential-image/$', ReferentialImageAPI.as_view(), name='referential_image'),
+    url(r'^v2/measurement-value/$', MeasurementValueV2API.as_view(), name='measurement_value_v2'),
+
 ]
